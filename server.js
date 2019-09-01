27ad64dc0170665a38b87a7f9b52394744478df2
Pat Palmerston <patpalmerston@gmail.com>
2019-08-31 22:43:37 -0700

    requests

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  const url = req.url;
+  if(url === '/') {
+    res.write('<html>');
+	res.write('<head><title> Enter Message </title></head>');
+	res.write('<body><form><input type="text"><button></button></form></body>');
+	res.write('<html>');
+	res.end();
+  }
 	// process.exit();
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html>');
