3fbdc2986b0dac379b230a7398afbcf28292e5a2
Pat Palmerston <patpalmerston@gmail.com>
2019-09-11 12:40:40 -0700

    blocking and non blocking code

--- server.js
+++ server.js
@@ -19,13 +19,15 @@ const server = http.createServer((req, res) => {
 			console.log(chunk);
 			body.push(chunk);
 		});
-		req.on('end', () => {
+		return req.on('end', () => {
 			const parsedBody = Buffer.concat(body).toString();
 			const message = parsedBody.split('=')[1];
-			fs.writeFileSync('message.text', message);
-			res.statusCode = 302;
-			res.setHeader('Location', '/');
-			return res.end();
+			fs.writeFile('message.text', message, err => {
+				console.log(err);
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+				return res.end();
+			});
 		});
 	}
 	res.setHeader('Content-Type', 'text/html');
