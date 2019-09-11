a2988140b80f98b9756d6179a09ebd7d65b65385
Pat Palmerston <patpalmerston@gmail.com>
2019-09-11 15:37:22 -0700

    first assignment

---
+++ assignment1/routes.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+	const url = req.url;
+	const method = req.method;
+
+	if (url === '/') {
+		res.write('<html>');
+		res.write('<head><title>Create a User</title></head>');
+		res.write(
+			'<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Create</button></form></body>'
+		);
+		res.write('<html>');
+		return res.end();
+	}
+	if (url === '/users' && method === 'POST') {
+		const body = [];
+		req.on('data', chunk => {
+			console.log(chunk);
+			body.push(chunk);
+		});
+		return req.on('end', () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split('=')[1];
+			fs.writeFile('message.text', message, err => {
+				console.log(err);
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+				return res.end();
+			});
+		});
+	}
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title> My First Page </title></head>');
+	res.write('<body><h1>List of Dummy Users</2h></body>');
+	res.write('<html>');
+	res.end();
+};
+
+module.exports = requestHandler;
