e01e7fabe9507ba676bff9202364145026c55a01
Pat Palmerston <patpalmerston@gmail.com>
2019-09-13 21:20:28 -0700

    express update

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('in the middleware');
-  next(); // allows the request to continue to the next middleware
+	console.log('in the middleware');
+	next(); // allows the request to continue to the next middleware
 });
 app.use((req, res, next) => {
-  console.log('in another middleware');
-  // ...
+	console.log('in another middleware');
+	res.send('<h1>Hello from express</h1>');
 });
 
 const server = http.createServer(app);
